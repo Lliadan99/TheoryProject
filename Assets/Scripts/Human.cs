d1aa98a172569cbd4dbc2622f6220442b58b32fc
Lliadan99 <111148092+Lliadan99@users.noreply.github.com>
2022-09-13 00:40:26 -0700

    added borders, primary objects for enemies, player
    scripts that will be used (I think) for inheritance, and polymorphism. scripts are still empty atm.  created a canvas for the score. It isn't the right size yet. I just put it there for reference.

---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+    private int _health;
+    private int _movementSpeed;
+
+
+    public virtual void Attack()
+    {
+
+    }
+
+}
